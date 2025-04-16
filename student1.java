4016326c72f82d6456637eb6f4e84802d44220cc
shivhanchin07 <ravihanchin@gmail.com>
2025-04-16 12:54:31 +0530

    student1

---
+++ student1.java
@@ -0,0 +1,19 @@
+package jenkins1;
+
+public class student1 { int rollno;
+    String name;
+    float fee;
+    student1(int rollno,String name,float fee)
+    {
+        rollno=rollno;
+        name=name;
+        fee=fee;
+    }
+    void display()
+    {
+        System.out.println(rollno+" "+name+" "+fee);
+    }
+}
+    
+    
+}
