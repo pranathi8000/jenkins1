89ae655824310fbf6a69888f02ebcdef70a324a8
shivhanchin07 <ravihanchin@gmail.com>
2025-04-16 12:34:37 +0530

    student2

---
+++ student2.java
@@ -0,0 +1,13 @@
+package jenkins1;
+
+class TestThis1{
+    public static void main(String args[])
+    {
+        student s1=new student(111,"ankit",5000f);
+        student s2=new student(112,"sumit",6000f);
+        s1.display();
+        s2.display();
+     
+    }
+    
+}
